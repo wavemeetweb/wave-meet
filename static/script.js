502786aaf8851bc09070d0a352c4cbfd161db9eb
dumaba <abhilash123happy@gmail.com>
2025-07-21 19:55:57 +0530

    Update script.js

--- static/script.js
+++ static/script.js
@@ -1,27 +1,8 @@
-const socket = io();
-let room = '';
-let localStream;
-const peers = {};
-
 function joinRoom() {
-  room = document.getElementById('roomInput').value;
-  socket.emit('join', { room });
-
-  navigator.mediaDevices.getUserMedia({ video: true, audio: true })
-    .then(stream => {
-      localStream = stream;
-      const localVideo = document.createElement('video');
-      localVideo.srcObject = stream;
-      localVideo.autoplay = true;
-      localVideo.muted = true;
-      document.getElementById('videos').appendChild(localVideo);
-    });
+  const room = document.getElementById("roomInput").value.trim();
+  if (room) {
+    window.location.href = `/room/${room}`;
+  } else {
+    alert("Please enter a valid room name.");
+  }
 }
-
-socket.on('user-joined', async ({ id }) => {
-  // Handle peer connection logic here (WebRTC signaling to be added)
-});
-
-socket.on('signal', data => {
-  // Signaling for WebRTC connection
-});
