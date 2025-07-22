d6c767a43badd760f868aba35f3b1a057fb536a7
dumaba <abhilash123happy@gmail.com>
2025-07-22 19:17:15 +0530

    Delete static directory

--- static/script.js
+++
@@ -1,8 +0,0 @@
-function joinRoom() {
-  const room = document.getElementById("roomInput").value.trim();
-  if (room) {
-    window.location.href = `/room/${room}`;
-  } else {
-    alert("Please enter a valid room name.");
-  }
-}
