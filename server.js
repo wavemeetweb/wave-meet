cfcda039b5447f052243c84003282369e059619d
wave <abhilash123happy@gmail.com>
2025-07-26 19:35:41 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,1 +1,28 @@
+// server.js
+const express = require("express");
+const http = require("http");
+const socketIo = require("socket.io");
+
+const app = express();
+const server = http.createServer(app);
+const io = socketIo(server, { cors: { origin: "*" } });
+
+io.on("connection", (socket) => {
+  socket.on("join-room", (roomId, userId) => {
+    socket.join(roomId);
+    socket.to(roomId).emit("user-connected", userId);
+
+    socket.on("disconnect", () => {
+      socket.to(roomId).emit("user-disconnected", userId);
+    });
+  });
+
+  socket.on("signal", (data) => {
+    io.to(data.roomId).emit("signal", data);
+  });
+});
+
+server.listen(5000, () => {
+  console.log("Signaling server running on port 5000");
+});
 
