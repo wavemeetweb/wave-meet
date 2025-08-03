73afdfc07834c0e0cfb3a68a0ee598957d54848e
wave <abhilash123happy@gmail.com>
2025-08-03 10:16:24 +0530

    Update HostMeeting.js

--- src/componenets/HostMeeting.js
+++ src/componenets/HostMeeting.js
@@ -1,1 +1,28 @@
+import React, { useState } from 'react';
+import { Box, TextField, Button, Typography, Paper } from '@mui/material';
+
+export default function HostMeeting() {
+  const [form, setForm] = useState({topic: '', description: '', password: ''});
+
+  const handleChange = e => setForm({...form, [e.target.name]: e.target.value});
+  const handleSubmit = e => {
+    e.preventDefault();
+    // TODO: Host meeting logic here
+    alert(`Meeting "${form.topic}" Created (Mock)`);
+  };
+
+  return (
+    <Box display="flex" justifyContent="center" alignItems="center" minHeight="80vh">
+      <Paper sx={{ padding: 4, minWidth: 350 }}>
+        <Typography variant="h5" gutterBottom>Host a Meeting</Typography>
+        <form onSubmit={handleSubmit}>
+          <TextField fullWidth label="Meeting Topic" name="topic" margin="normal" value={form.topic} onChange={handleChange} required />
+          <TextField fullWidth label="Description" name="description" margin="normal" value={form.description} onChange={handleChange} />
+          <TextField fullWidth label="Meeting Password" name="password" margin="normal" type="password" value={form.password} onChange={handleChange} />
+          <Button variant="contained" type="submit" fullWidth sx={{ mt: 2 }}>Create Meeting</Button>
+        </form>
+      </Paper>
+    </Box>
+  );
+}
 
