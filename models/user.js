beb67ed33ff549f5fc79cb790613a91219f967f8
wave <abhilash123happy@gmail.com>
2025-08-12 19:32:09 +0530

    Update user.js

--- models/user.js
+++ models/user.js
@@ -1,1 +1,18 @@
+const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
+
+const userSchema = new mongoose.Schema({
+  username: { type: String, unique: true, required: true },
+  password: { type: String, required: true }
+});
+
+// Hash before saving
+userSchema.pre('save', async function(next) {
+  if (!this.isModified('password')) return next();
+  const salt = await bcrypt.genSalt(10);
+  this.password = await bcrypt.hash(this.password, salt);
+  next();
+});
+
+module.exports = mongoose.model('User', userSchema);
 
